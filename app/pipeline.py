af6810154265c0acfec88114662c1904d88826a5
Aidan Hoo <achrunaway@gmail.com>
2026-04-09 21:37:22 -0400

    bump

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,25 +1,11 @@
 """Orchestator"""
 
-import torch
-from pyannote.audio import Pipeline
-from pyannote.audio.pipelines.utils.hook import ProgressHook
+import argparse
 
-# Community-1 open-source speaker diarization pipeline
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-community-1",
-    token="HUGGINGFACE_ACCESS_TOKEN")
+def main():
+    parser = argparse.ArgumentParser(
+        description=""
+    )
 
-# send pipeline to GPU (when available)
-pipeline.to(torch.device("cuda"))
-
-# apply pretrained pipeline (with optional progress hook)
-with ProgressHook() as hook:
-    output = pipeline("audio.wav", hook=hook)  # runs locally
-
-# print the result
-for turn, speaker in output.speaker_diarization:
-    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
-# start=0.2s stop=1.5s speaker_0
-# start=1.8s stop=3.9s speaker_1
-# start=4.2s stop=5.7s speaker_0
-# ...
+if __name__ == "__main__":
+    main()
